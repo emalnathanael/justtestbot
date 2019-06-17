a0dd8280a806180f60a89f81a8c264fafa82c85a
My Name Is Mr Twin's <36656233+HebatLuBoy@users.noreply.github.com>
2019-06-17 17:24:02 +0700

    Create toolss.sh

---
+++ toolss.sh
@@ -0,0 +1,58 @@
+sleep 2
+toilet -f standard -F gay "Wellcome to"
+toilet -f standard -F gay "Pkg installer"
+echo "========================================"
+
+echo "  #Coded   : Mr Twin's"
+sleep 1
+echo "  #Team    : ImN0tHacker"
+sleep 1
+echo "  #Contact : GAUSAH DI KONTAK KONTOL"
+sleep 1
+echo "========================================"
+sleep 3
+echo "[+] Tunggu sebentar.."
+sleep 7
+
+
+pkg install python2 -y
+pkg install php -y
+pkg install lolcat -y
+pkg install unzip -y
+pkg install ruby -y
+pkg install mcedit -y
+pkg install nano -y
+pkg install bash -y
+pkg install git -y
+pkg install nodejs -y
+pkg install python -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install ruby -y
+pkg install sl
+pkg install figlet -y
+pip2 install toilet -y
+pip2 install mechanize -y
+pip2 install lolcat -y
+pkg install mkdir -y
+pkg install ubduco -y
+pkg install abook -y
+pkg install abuild -y
+pkg install ack-grep -y
+pkg install alpine -y
+pkg install apache2 -y
+pkg install apk-tools -y
+pkg install apksigner -y
+pkg install apr-util -y
+pkg install apr -y
+pkg install argp -y
+pkg install aria2 -y
+pkg install asciinema -y
+pkg install cmatrix -y
+cmatrix
+sleep 1
+toilet -f standard -F gay "Selesai"
+sleep 1
+echo"[+] Nanti Tunggu Updatenya ya.. biar w tambahain yang banyak"
+sleep 5
+exit 
