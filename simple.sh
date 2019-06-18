0e6fecaadf08122df395f1f2f27d75dc074a0894
My Name Is Mr Twin's <36656233+HebatLuBoy@users.noreply.github.com>
2019-06-18 13:33:41 +0700

    Create simple.sh

---
+++ simple.sh
@@ -0,0 +1,37 @@
+sleep 2
+toilet -f standard -F gay "Wellcome to"
+toilet -f standard -F gay "Pkg installer"
+echo "========================================"
+
+echo "  #Coded   : Mr HebatLuBoy"
+sleep 1
+echo "  #Team    : Im N0t Hacker"
+sleep 1
+echo "  #Contact : GAUSAH DI KONTAK KONTOL"
+sleep 1
+echo "========================================"
+sleep 3
+echo "[+] Tunggu sebentar.."
+sleep 7
+
+pkg update && pkg upgrade
+pkg install python2 -y
+pkg install php -y
+pkg install lolcat -y
+pkg install nano -y
+pkg install bash -y
+pkg install git -y
+pkg install python -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install sl
+pkg install figlet -y
+pip2 install toilet -y
+pip2 install lolcat -y
+pkg install cmatrix -y
+sleep 1
+toilet -f standard -F gay "Selesai"
+sleep 1
+echo"[+] Nanti Tunggu Updatenya ya.. biar w tambahain yang banyak"
+sleep 5
+exit 
