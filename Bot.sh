940ec6648468f3bb0e1799eeac2c3413f777bf33
My Name Is Mr Twin's <36656233+HebatLuBoy@users.noreply.github.com>
2021-01-10 19:34:31 +0000

    Update and rename simple.sh to Bot.sh

---
+++ Bot.sh
@@ -0,0 +1,43 @@
+sleep 2
+toilet -f standard -F gay "Wellcome to"
+toilet -f standard -F gay "Pkg installer"
+echo "========================================"
+
+echo "  #Coded   : Mr HebatLuBoy"
+sleep 1
+echo "  #Team    : GUA BUKAN KICKER NGENTOD"
+sleep 1
+echo "  #TITTLE : INSTALL SELFBOT"
+sleep 1
+echo "========================================"
+sleep 3
+echo "[+] Tunggu sebentar.."
+sleep 7
+
+pkg install python3 -y
+apt-get install python --upgrade
+pkg install git -y
+pkg install nano -y
+pip3 install rsa
+pip3 install thrift==0.11.0
+pip3 install requests
+pip3 install bs4
+pip3 install gtts
+pip3 install beautifulsoup
+pip3 install googletrans
+pip3 install pafy
+pip3 install humanfriendly
+pip3 install goslate
+pip3 install wikipedia
+pip3 install youtube_dl
+pip3 install tweepy
+pip3 install pytz
+pip3 install html5lib
+pip3 install pafy
+git clone https://github.com/dhenza1415/SELFBOT1
+sleep 1
+toilet -f standard -F gay "Selesai"
+sleep 1
+echo"[+] Nanti Tunggu Updatenya ya.. biar w tambahain yang banyak"
+sleep 5
+exit 
